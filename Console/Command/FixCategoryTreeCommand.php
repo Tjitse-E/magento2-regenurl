3f71c7daa60eea7840ae66a404f81b0fa31d1282
Tjitse <Tjitse@vendic.nl>
2018-07-20 16:50:08 +0200

    Add command regenerate:category:tree (#1)
    * Move Iazel/RegenProductUrl to repo root
    
    * Add FixCategoryTree command
    
    * Switched to running RegenerateCategoryPathCommand and RegenerateCategoryUrlCommand directly
    
    * Applied php-cs-fixer
    
    * Add option descriptions
    
    * Add extra comments, style improvements, remove unused imports
    
    * Add cache clean + reindex optional inputs
    
    * Fix registration.php path
    
    * Fix composer php requirements
    
    * Fix PSR-4
    
    * Fix cache/reindex default option
    
    * Update docs
    
    * Update README.md
    
    * Update readme
    
    * Update readme

---
+++ Console/Command/FixCategoryTreeCommand.php
@@ -0,0 +1,296 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @author Tjitse (Vendic)
+ * Created on 20-07-18 11:43
+ */
+
+namespace Iazel\RegenProductUrl\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\App\Emulation;
+use Magento\Framework\EntityManager\EventManager;
+use Magento\Framework\App\State;
+use Magento\UrlRewrite\Model\UrlPersistInterface;
+use Magento\CatalogUrlRewrite\Model\CategoryUrlRewriteGenerator;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Store\Model\StoreManagerInterface;
+
+class FixCategoryTreeCommand extends Command
+{
+    /**
+     * @var CategoryRepositoryInterface
+     */
+    protected $categoryRepository;
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+    /**
+     * @var CollectionFactory
+     */
+    protected $categoryCollectionFactory;
+    /**
+     * @var ResourceConnection
+     */
+    protected $connection;
+    /**
+     * @var EventManager
+     */
+    protected $eventManager;
+    /**
+     * @var Emulation
+     */
+    protected $emulation;
+    /**
+     * @var State
+     */
+    protected $state;
+    /**
+     * @var UrlPersistInterface
+     */
+    protected $urlPersist;
+    /**
+     * @var CategoryUrlRewriteGenerator
+     */
+    protected $categoryUrlRewriteGenerator;
+
+    public function __construct(
+        CategoryUrlRewriteGenerator $categoryUrlRewriteGenerator,
+        UrlPersistInterface $urlPersist,
+        State $state,
+        EventManager $eventManager,
+        Emulation $emulation,
+        ResourceConnection $connection,
+        CollectionFactory $categoryCollectionFactory,
+        StoreManagerInterface $storeManager,
+        CategoryRepositoryInterface $categoryRepository
+    ) {
+        $this->categoryRepository = $categoryRepository;
+        $this->storeManager = $storeManager;
+        parent::__construct();
+        $this->categoryCollectionFactory = $categoryCollectionFactory;
+        $this->connection = $connection;
+        $this->eventManager = $eventManager;
+        $this->emulation = $emulation;
+        $this->state = $state;
+        $this->urlPersist = $urlPersist;
+        $this->categoryUrlRewriteGenerator = $categoryUrlRewriteGenerator;
+    }
+
+    protected function configure()
+    {
+        $this->setName('regenerate:category:tree')
+            ->setDescription('Regenerate the category tree for a given category tree')
+            ->addOption(
+                'category',
+                'c',
+                InputOption::VALUE_REQUIRED,
+                'Category tree to regenerate (parent category including its children)'
+            )
+            ->addOption(
+                'store',
+                's',
+                InputOption::VALUE_REQUIRED,
+                'Magento Store ID',
+                Store::DEFAULT_STORE_ID
+            )->addOption(
+                'reindex',
+                'r',
+                InputOption::VALUE_OPTIONAL,
+                'Reindex after completion?',
+                false
+            )
+            ->addOption(
+                'flush',
+                'f',
+                InputOption::VALUE_OPTIONAL,
+                'Cache flush after completion?',
+                false
+            );
+        return parent::configure();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Get and check input
+        $categoryId = $input->getOption('category');
+        $storeId = $input->getOption('store');
+        if (empty($categoryId) || empty($storeId)) {
+            $output->writeln("<error>Category ID or Store ID missing</error>");
+            return;
+        }
+
+        // Get categorie and store
+        try {
+            $category = $this->categoryRepository->get($categoryId);
+        } catch (\Magento\Framework\Exception\NoSuchEntityException $exception) {
+            $output->writeln("<error>Category: {$exception->getMessage()}</error>");
+            return;
+        }
+        try {
+            $store = $this->storeManager->getStore($storeId);
+        } catch (\Magento\Framework\Exception\NoSuchEntityException $exception) {
+            $output->writeln("<error>Store: {$exception->getMessage()}</error>");
+            return;
+        }
+
+        // Start fixing tree
+        $output->writeln("<info>Starting regeneration for {$category->getName()} in store {$store->getCode()}</info>");
+
+        // Check descendants
+        $descendants = $this->getDescendants($category, $storeId);
+        if (!$descendants) {
+            $output->writeln("<error>No descendants found for {$category->getName()}</error>");
+            return;
+        }
+
+        // Debugging, render child category tables
+        $this->renderChildTable($output, $descendants);
+
+        // Get all child category ids from parent, including parent category.
+        $categoryIds = $this->getAllCategoryIds($category, $descendants);
+
+        // Delete old redirects
+        $categoryIdsString = implode(', ', $categoryIds);
+        $output->writeln("<info>1. Deleting old redirects for {$categoryIdsString}. SQL query:</info>");
+        $this->deleteUrlRewriteRecords($output, $categoryIds, $store);
+
+        // Acivate RegenerateCategoryPathCommand
+        $output->writeln("<info>2. Starting path regeneration. Executing 'regenerate:category:path'</info>");
+        $arguments = new ArrayInput(['cids' => $categoryIds, '--store' => $storeId]);
+        $this->getApplication()->find('regenerate:category:path')->run($arguments, $output);
+
+        // Activate RegenerateCategoryUrlCommand
+        $output->writeln("<info>3. Starting url regeneration. Executing 'regenerate:category:url'</info>");
+        $this->getApplication()->find('regenerate:category:url')->run($arguments, $output);
+
+        // Optional cache clean and reindex
+        $this->reindex($input, $output);
+        $this->cacheFlush($input, $output);
+
+        $output->writeln('<info>Finshed!</info>');
+    }
+
+    /**
+     * @param $category
+     * @param int $levels
+     * @return \Magento\Catalog\Model\ResourceModel\Category\Collection|false
+     */
+    public function getDescendants($category, $storeId, $levels = 10)
+    {
+        if ((int)$levels < 1) {
+            $levels = 1;
+        }
+        $collection = $this->categoryCollectionFactory->create()
+            ->addAttributeToSelect(['name', 'url_key'])
+            ->addPathsFilter($category->getPath() . '/')
+            ->addLevelFilter($category->getLevel() + $levels)
+            ->setStore($storeId);
+
+        if ($collection->count() === 0) {
+            return false;
+        }
+
+        return $collection;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param $descendants
+     */
+    protected function renderChildTable(OutputInterface $output, $descendants): void
+    {
+        $rows = [];
+        foreach ($descendants->getItems() as $descendant) {
+            /**
+             * @var $descendant \Magento\Catalog\Model\Category
+             */
+            $rows[] = [$descendant->getId(), $descendant->getName(), $descendant->getUrlKey()];
+        }
+        $output->writeln("<info>{$descendants->count()} child categories found.</info>");
+        // Output child categories
+        $table = new Table($output);
+        $table
+            ->setHeaders(['ID', 'Name', 'Url Key'])
+            ->setRows($rows)
+            ->render();
+    }
+
+    /**
+     * @param $category
+     * @param $descendants
+     * @return array
+     */
+    protected function getAllCategoryIds($category, $descendants): array
+    {
+        $categoryIds = [$category->getId()];
+        $categories = $descendants->getItems();
+        foreach ($categories as $childCategory) {
+            $categoryIds[] = $childCategory->getId();
+        }
+        return $categoryIds;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param $categoryIds
+     * @param $store
+     */
+    protected function deleteUrlRewriteRecords(OutputInterface $output, $categoryIds, $store): void
+    {
+        $connection = $this->connection->getConnection();
+        $categoryIdsString = implode(', ', $categoryIds);
+        $sql = "DELETE FROM `url_rewrite` WHERE `entity_id` IN ({$categoryIdsString}) AND `entity_type` = 'category' AND `store_id` = {$store->getId()}";
+        $output->writeln($sql);
+        $connection->query($sql);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return ArrayInput
+     */
+    protected function reindex(InputInterface $input, OutputInterface $output): void
+    {
+        if ($input->getOption('reindex')) {
+            $output->writeln('<info>Starting reindex</info>');
+
+            $arguments = new ArrayInput(
+                [
+                    'command' => 'index:reindex',
+                    'index' => [
+                        'catalog_product_flat',
+                        'catalog_category_flat',
+                        'catalog_category_product',
+                        'catalog_product_category',
+                        'catalog_product_attribute'
+                    ]
+                ]
+            );
+            $this->getApplication()->find('index:reindex')->run($arguments, $output);
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function cacheFlush(InputInterface $input, OutputInterface $output): void
+    {
+        if ($input->getOption('flush')) {
+            $output->writeln('<info>Starting cache clean</info>');
+            $arguments = new ArrayInput(['command' => 'cache:flush']);
+            $this->getApplication()->find('cache:flush')->run($arguments, $output);
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,58 @@
+# Magento 2 - Fix broken multistore category urls
+
+This fork of [peterjaap/magento2-regenurl](https://github.com/peterjaap/magento2-regenurl) adds a command to fix broken category urls on Magento 2.2.x. You can find more info about the issue here: [#16202](https://github.com/magento/magento2/issues/16202)
+
+**We've used this solution once, on one of our Magento stores running Magento 2.2.4, with over 30 store views using four languages. Never try this on production without testing!**
+
+## The problem
+
+When you're running a Magento 2.2 store with multiple store views, let's say three stores in different languages (Dutch, French and German), sometimes the category urls will start to deform like this:.
+
+- www.store.de/dutch-word/german-word
+- www.store.fr/german-word/french-word
+- www.store.nl/french-word/dutch-word
+
+### The solution
+We've tested this solution on one of our Magento stores running Magento 2.2.4, with over 30 store views using four languages
+
+1. Identify the problem by running the following SQL query:
+```sql
+SELECT * FROM `url_rewrite` WHERE `entity_type` LIKE 'category' AND `entity_id` IN (category, ids, here) AND `store_id` = 8 ORDER BY `url_rewrite_id`
+```
+In our case this resulted in multiple bad 301 redirects, plus the wrong category urls.
+2. Delete the url_rewrites:
+```sql
+DELETE FROM `url_rewrite` WHERE `entity_type` LIKE 'category' AND `entity_id` IN (category, ids, here) AND `store_id` = 8 ORDER BY `url_rewrite_id`
+```
+3. Run the command `php bin/magento regenerate:product:path -s storeId cids 1 2 3 5`
+4. Run the command `php bin/magento regenerate:product:url -s storeId cids 1 2 3 5`
+
+### Automating the solution
+I've created a command that will do this for you automatically:
+
+`php bin/magento regenerate:category:tree --category categoryId --store storeId`
+
+In our case it didn't work to regenerate one of the deepest level subcategories. We had to use one of the top level categories to make this work. The command will automatically fix the parent and the child categories.
+
+Output:
+
+![Output screenshot](https://github.com/Tjitse-E/magento2-regenurl/blob/develop/docs/console-screenshot.jpg)
+
+Before:
+
+![Before screenshot](https://github.com/Tjitse-E/magento2-regenurl/blob/develop/docs/wrong-urls.png)
+
+After:
+
+![After screenshot](https://github.com/Tjitse-E/magento2-regenurl/blob/develop/docs/fixed-urls.png)
+
+# Install
+Using Composer;
+
+```sh
+composer config repositories.regenurl vcs git@github.com:Tjitse-E/magento2-regenurl.git
+composer require iazel/module-regen-product-url
+php bin/magento setup:upgrade
+```
+
+Or download and copy the `Iazel` directory into `app/code/` and run `php bin/magento setup:upgrade`.
